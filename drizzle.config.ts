c7b1bc1d6b8faa36a3797601396ec4196e017e25
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-12-08 16:25:53 +0100

    fix (drizzle): added casing to drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   schema: './src/database/lib/schema.ts',
   out: './src/database/migrations',
   dialect: 'postgresql',
+  casing: 'snake_case',
   dbCredentials: {
     url: process.env.DATABASE_URL as string,
   },
